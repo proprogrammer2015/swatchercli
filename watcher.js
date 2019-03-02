07bb610a23222d7d9d5a646ae350e62efad0465c
Jacek Mankowski <it.developer.1992@gmail.com>
2019-03-02 21:30:26 +0100

    Refactoring

--- watcher.js
+++ watcher.js
@@ -22,6 +22,10 @@ const defaultProcessors = [
     new Css()
 ];
 
+const mapRequiredProcessors = processors => processors
+    .filter(type => type.isRequired())
+    .map(type => `.${type.extension()}`);
+
 exports.watcher = (
     {
         isSingleRun,
@@ -42,14 +46,10 @@ exports.watcher = (
         }
     };
 
-    // const requiredExtensions = processors
-    //     .filter(type => type.isRequired())
-    //     .map(type => `.${type.extension()}`);
-    const requiredExtensions = ['.html'];
+    const requiredExtensions = mapRequiredProcessors(processors);
 
     const sc = new SvelteCombine(fs, { output, processors });
-
-    const compile = (sc, relativePath) => {
+    const compile = relativePath => {
         try {
             sc.combine(relativePath);
             logSuccess(`${relativePath} was saved`);
@@ -58,44 +58,38 @@ exports.watcher = (
         }
     };
 
-    const onChanged = relativePath => compile(sc, relativePath)
-
     const onDeleted = relativePath => {
         const { dir, modulePath, name, ext } = resolvePath(relativePath, output);
         const toBeRemoved = `${dir.join('/')}/${name}${ext}`;
         rimraf.sync(toBeRemoved);
         logSuccess(`${toBeRemoved} was deleted`);
 
-        const moduleLeftoverPaths = relativePaths(watcher.getWatched())
-            .filter(p => p.indexOf(`${modulePath}/${name}`) !== -1);
-
-        if (moduleLeftoverPaths.length) {
-            // Recompile if part of the module was removed
-            moduleLeftoverPaths.forEach(relativePath => compile(sc, relativePath));
-        }
+        // Recompile if part of the module was removed
+        relativePaths(watcher.getWatched())
+            .filter(path => path.indexOf(`${modulePath}/${name}`) !== -1)
+            .forEach(compile);
     };
     const onRelative = callback => filepath => callback(replaceSlashes(filepath));
 
+    const onAdd = newRelativePath => {
+        const isNew = relativePaths(watcher.getWatched())
+            .some(relativePath => relativePath !== newRelativePath);
+
+        if (isNew) {
+            compile(newRelativePath);
+        }
+    };
     const watcher = chokidar.watch(patterns, { persistent: !isSingleRun, cwd: process.cwd() });
     watcher
-        .on('change', onRelative(onChanged))
+        .on('change', onRelative(compile))
         .on('unlink', onRelative(onDeleted))
         .on('unlinkDir', onRelative(onDeleted))
         .on('error', logError)
         .on('ready', () => {
             relativePaths(watcher.getWatched())
                 .filter(matchSome(requiredExtensions))
-                .forEach(filepath => compile(sc, filepath))
+                .forEach(compile);
 
-            watcher.on('add', path => {
-                const newRelativePath = replaceSlashes(path);
-                const isNew = relativePaths(watcher.getWatched())
-                    .filter(relativePath => relativePath !== newRelativePath)
-                    .some(isNew => !!isNew);
-
-                if (isNew) {
-                    onChanged(newRelativePath);
-                }
-            })
+            watcher.on('add', onRelative(onAdd));
         });
 }
